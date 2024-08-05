3484d04f14c0618fd55be3335157fc2051f1b51f
svenwong <illust0130@foxmail.com>
2024-08-05 10:53:36 +0800

    updated tabbar

--- src/components/BottomNav/index.vue
+++ src/components/BottomNav/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <van-tabbar v-model="active" route>
+  <van-tabbar v-model="active" route class="bottom">
     <van-tabbar-item icon="records-o" to="/home">填字</van-tabbar-item>
     <van-tabbar-item icon="browsing-history-o" to="/history">往期</van-tabbar-item>
     <van-tabbar-item icon="fire-o" to="/explore">探索</van-tabbar-item>
@@ -13,4 +13,24 @@ import { ref } from 'vue'
 const active = ref(0)
 </script>
 
-<style scoped></style>
+<style scoped>
+.bottom {
+  bottom: 10px;
+  width: 360px;
+  margin: 0 7px 0 7px;
+  background-color: rgb(40, 187, 245);
+  border-radius: 8px;
+  box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.5);
+}
+:deep(.van-tabbar-item--active) {
+  color: white;
+  background-color: rgb(40, 187, 245);
+}
+:deep(.van-tabbar-item) {
+  border-radius: 8px;
+  background-color: rgb(40, 187, 245);
+}
+.bottom:after {
+  border-width: 0;
+}
+</style>
